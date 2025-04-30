5b2b3c72cc0ae980a3e4a474f6b16b9a6d86682a
Maurice Preuß (envoyr) <hello@envoyr.com>
2025-04-30 04:23:10 +0200

    add serializable
    Signed-off-by: Maurice Preuß (envoyr) <hello@envoyr.com>

--- src/Id/Resources/BaseResource.php
+++ src/Id/Resources/BaseResource.php
@@ -4,13 +4,30 @@ namespace Anikeen\Id\Resources;
 
 use Anikeen\Id\Concerns\MagicProperties;
 use Anikeen\Id\Result;
+use JsonSerializable;
 
-abstract class BaseResource
+abstract class BaseResource implements JsonSerializable
 {
     use MagicProperties;
 
     public function __construct(protected Result $result)
     {
         $this->setMagicProperties($this->result->data);
+    }
+
+    /**
+     * Returns the collection of resources as an array.
+     */
+    public function toArray(): array
+    {
+        return (array)$this->result->data;
+    }
+
+    /**
+     * Returns the collection of resources as a JSON string.
+     */
+    public function jsonSerialize(): array
+    {
+        return $this->toArray();
     }
 }
