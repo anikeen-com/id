0f14fa1b4cb466c361555916e14bc57379357c71
Maurice Preuß <hello@envoyr.com>
2025-09-06 13:32:06 +0000

    remove endpoint
    Signed-off-by: Maurice Preuß <hello@envoyr.com>

--- src/Id/Resources/Transactions.php
+++ src/Id/Resources/Transactions.php
@@ -11,35 +11,6 @@ class Transactions extends BaseCollection
     use HasBillable;
 
     /**
-     * Create a new transaction for the current user.
-     *
-     * @param array{
-     *       group: null|string,
-     *       invoice_id: null|string,
-     *       payment_provider: null|string,
-     *       payment_intent: null|string,
-     *       status: string,
-     *       type: string,
-     *       amount: float,
-     *       created_at: string
-     *   } $attributes The attributes for the transaction.
-     *     - group:            The group (optional)
-     *     - invoice_id:       The invoice id (optional)
-     *     - payment_provider: The payment provider (optional, e.g. "kofi", "stripe")
-     *     - payment_intent:   The payment intent (optional)
-     *     - status:           The status (e.g. "expired", "failed", "pending", "refunded", "succeeded")
-     *     - type:             The type (e.g. "deposit", "withdrawal")
-     *     - amount:           The amount
-     *     - created_at:       The created at datetime string (e.g. "Y-M-D H:i:s")
-     * @throws Throwable
-     */
-    public function create(array $attributes = []): Transaction
-    {
-        return (new Transaction($this->billable->request('POST', 'v1/transactions', $attributes)))
-            ->setBillable($this->billable);
-    }
-
-    /**
      * {@inheritDoc}
      */
     public function find(string $id): ?Transaction
